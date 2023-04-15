90654b333342718def7aee3b1b955d2c32e03326
montrells <montrell.story@gmail.com>
2023-04-15 11:37:07 +0300

    Annotation demo

--- src/main/java/platform/codingnomads/co/corespring/examples/annotations/whatandwhy/AnnotationDemoService.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/annotations/whatandwhy/AnnotationDemoService.java
@@ -3,8 +3,15 @@ package platform.codingnomads.co.corespring.examples.annotations.whatandwhy;
 public class AnnotationDemoService implements LegacyInfoProvider {
 
     @ModernInfo
+    @SecondaryData
     @Override
     public String info() {
         return "legacy api fetching information";
     }
+
+    @Override
+    public String data() {
+        return "primary data";
+    }
 }
+
--- src/main/java/platform/codingnomads/co/corespring/examples/annotations/whatandwhy/AnnotationParsingDemo.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/annotations/whatandwhy/AnnotationParsingDemo.java
@@ -7,9 +7,9 @@ public class AnnotationParsingDemo {
         try {
             Class<AnnotationDemoService> annotationDemoService = AnnotationDemoService.class;
             for (Method method : annotationDemoService.getMethods()) {
-                if (method.isAnnotationPresent(ModernInfo.class)) {
-                    ModernInfo modernInfo = method.getAnnotation(ModernInfo.class);
-                    System.out.println("Info Received: " + modernInfo.info());
+                if (method.isAnnotationPresent(SecondaryData.class)) {
+                    SecondaryData modernInfo = method.getAnnotation(SecondaryData.class);
+                    System.out.println("Info Received: " + modernInfo.data());
                 }
             }
         } catch (Exception e) {
