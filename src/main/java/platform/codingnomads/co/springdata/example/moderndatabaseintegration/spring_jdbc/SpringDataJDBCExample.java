566f327540b6f3927902caab4485da875682c921
montrells <montrell.story@gmail.com>
2023-05-06 13:04:25 +0300

    Changed out the employee to manger

--- src/main/java/platform/codingnomads/co/springdata/example/moderndatabaseintegration/spring_jdbc/SpringDataJDBCExample.java
+++ src/main/java/platform/codingnomads/co/springdata/example/moderndatabaseintegration/spring_jdbc/SpringDataJDBCExample.java
@@ -28,7 +28,7 @@ public class SpringDataJDBCExample implements CommandLineRunner {
     public void run(String... strings) {
 
         try {
-            //create employee table using the JdbcTemplate method "execute"
+            //create manager table using the JdbcTemplate method "execute"
             jdbcTemplate.execute("CREATE TABLE managers (id BIGINT AUTO_INCREMENT PRIMARY KEY," +
                     "first_name VARCHAR(255) NOT NULL,last_name  VARCHAR(255) NOT NULL);");
         } catch (Exception e) {
@@ -40,18 +40,18 @@ public class SpringDataJDBCExample implements CommandLineRunner {
                 .map(name -> name.split(" "))
                 .collect(Collectors.toList());
 
-//        for each first & last name pair insert an Employee into the database
+//        for each first & last name pair insert an Manager into the database
         for (Object[] name : splitUpNames) {
-            jdbcTemplate.execute(String.format("INSERT INTO employees(first_name, last_name) VALUES ('%s','%s')", name[0], name[1]));
+            jdbcTemplate.execute(String.format("INSERT INTO managers(first_name, last_name) VALUES ('%s','%s')", name[0], name[1]));
         }
 
-        //query the database for Employees with first name Java
+        //query the database for Managers with first name Java
         jdbcTemplate.query(
                         "SELECT id, first_name, last_name FROM managers WHERE first_name = 'Java'",
-                        (rs, rowNum) -> new Employee((int) rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+                        (rs, rowNum) -> new Manager((int) rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
                 )
-                //print each found employee to the console
-                .forEach(employee -> System.out.println(employee.toString()));
+                //print each found manager to the console
+                .forEach(manager -> System.out.println(manager.toString()));
 
        // truncate the table
         jdbcTemplate.execute("TRUNCATE TABLE managers;");
