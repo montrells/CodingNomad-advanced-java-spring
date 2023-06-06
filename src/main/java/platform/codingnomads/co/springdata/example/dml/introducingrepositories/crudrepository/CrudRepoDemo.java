e610b7eed6c6f34bf9e833d2b24d35b83e10e556
montrells <montrell.story@gmail.com>
2023-06-06 12:52:42 +0300

    Modified the CrudRepoDemo.java

--- src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/crudrepository/CrudRepoDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/crudrepository/CrudRepoDemo.java
@@ -25,10 +25,16 @@ public class CrudRepoDemo implements CommandLineRunner {
         //create new user
         User user = User.builder().firstName("Bobby").lastName("Bobbert").age(56).build();
         User user2 = User.builder().firstName("Joanne").lastName("Joanna").age(36).build();
+        User user3 = User.builder().firstName("Montrell").lastName("Story").age(45).build();
+        User user4 = User.builder().firstName("Roger").lastName("Jones").age(55).build();
+        User user5 = User.builder().firstName("Rita").lastName("Hamilton").age(29).build();
 
         //save user and assign what is returned to the user variable.
         user = userRepo.save(user);
         user2 = userRepo.save(user2);
+        user3 = userRepo.save(user3);
+        user4 = userRepo.save(user4);
+        user5 = userRepo.save(user5);
 
         Iterable<User> users = userRepo.findAll();
 
@@ -39,5 +45,8 @@ public class CrudRepoDemo implements CommandLineRunner {
         //delete the user using the id of the inserted user object
         userRepo.deleteById(user.getId());
         userRepo.deleteById(user2.getId());
+        userRepo.deleteById(user3.getId());
+        userRepo.deleteById(user4.getId());
+        userRepo.deleteById(user5.getId());
     }
 }
